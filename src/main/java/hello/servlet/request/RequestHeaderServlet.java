b33b9b2d2398de0255f99d5adc32a19bac90dee0
yeji.jo <choyeji1591@gmail.com>
2023-06-05 16:50:36 +0900

    feat : httpServletRequest, httpSevletResponse

---
+++ src/main/java/hello/servlet/request/RequestHeaderServlet.java
@@ -0,0 +1,44 @@
+package hello.servlet.request;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.Enumeration;
+
+@WebServlet(name = "requestHeaderServlet", urlPatterns = "/request-header")
+public class RequestHeaderServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) {
+        printStartLine(request);
+        printHeaders(request);
+    }
+
+    private static void printHeaders(HttpServletRequest request) {
+        // 방법1
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String headerName = headerNames.nextElement();
+            System.out.println("headerName = " + headerName);
+        }
+        // 방법2
+        request.getHeaderNames().asIterator().forEachRemaining(headerName -> System.out.println("headerName = " + headerName));
+    }
+
+    private static void printStartLine(HttpServletRequest request) {
+        System.out.println("--- REQUEST-LINE - start ---");
+        System.out.println("request.getMethod() = " + request.getMethod()); //GET
+        System.out.println("request.getProtocol() = " + request.getProtocol()); //
+        System.out.println("request.getScheme() = " + request.getScheme()); //http
+        // http://localhost:8080/request-header
+        System.out.println("request.getRequestURL() = " + request.getRequestURL());
+        // /request-header
+        System.out.println("request.getRequestURI() = " + request.getRequestURI());
+        //username=hi
+        System.out.println("request.getQueryString() = " + request.getQueryString());
+        System.out.println("request.isSecure() = " + request.isSecure()); //https
+        System.out.println("--- REQUEST-LINE - end ---");
+        System.out.println();
+    }
+}
