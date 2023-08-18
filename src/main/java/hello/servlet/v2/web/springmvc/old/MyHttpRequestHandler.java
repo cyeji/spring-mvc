9ce57ab9c60744f788319ebef31982ab53ebee44
cyeji <choyeji1591@gmail.com>
2023-08-18 23:49:25 +0900

    feat : 핸들러 매핑과 핸들러 어댑터

---
+++ src/main/java/hello/servlet/v2/web/springmvc/old/MyHttpRequestHandler.java
@@ -0,0 +1,20 @@
+package hello.servlet.v2.web.springmvc.old;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.HttpRequestHandler;
+
+@Slf4j
+@Component("/springmvc/request-handler")
+public class MyHttpRequestHandler implements HttpRequestHandler {
+
+    @Override
+    public void handleRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        log.info("myHttpRequestHandler.handleRequest");
+    }
+    
+}
